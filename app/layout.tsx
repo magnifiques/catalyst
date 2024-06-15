eebb2258693d2b414d0d397510329967c7ff81e4
Arpit <arpit.vaghela@outlook.com>
2024-06-14 22:11:15 -0400

    setting up the code

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+  title: "Catalyst",
+  description: "A Platform for events",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.variable}>{children}</body>
+    </html>
+  );
+}

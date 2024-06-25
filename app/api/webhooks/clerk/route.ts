0dd7701e83c33cdc32e52af94f0c179afbb38327
Arpit <arpit.vaghela@outlook.com>
2024-06-25 16:15:59 -0400

    another types change

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -67,9 +67,10 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name!,
-      lastName: last_name,
+      lastName: last_name!,
       photo: image_url,
     };
+
     const newUser = await createUser(user);
     console.log("api console", newUser);
 
@@ -89,7 +90,7 @@ export async function POST(req: Request) {
 
     const user = {
       firstName: first_name!,
-      lastName: last_name,
+      lastName: last_name!,
       username: username!,
       photo: image_url,
     };

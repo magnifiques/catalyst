33203b3f60fd2284d5dbc30ba294f32a2d5cd396
Arpit <arpit.vaghela@outlook.com>
2024-06-25 16:31:37 -0400

    remove the typescript checker

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
@@ -66,13 +67,12 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name,
+      lastName: last_name,
       photo: image_url,
     };
 
     const newUser = await createUser(user);
-    console.log("api console", newUser);
 
     if (newUser) {
       await clerkClient.users.updateUserMetadata(id, {
@@ -89,8 +89,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name,
+      lastName: last_name,
       username: username!,
       photo: image_url,
     };
